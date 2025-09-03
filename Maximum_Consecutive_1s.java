ae27e2097df93fa364ec24050a2aecf966f86f1c
Shaileshk-cloud <shailugowda398@gmail.com>
2025-09-03 09:12:00 +0530

    Update Maximum_Consecutive_1s.java

--- Maximum_Consecutive_1s.java
+++ Maximum_Consecutive_1s.java
@@ -1,5 +1,17 @@
 import java.util.Scanner;
+Given a binary array nums, return the maximum number of consecutive 1's in the array.
 
+ 
+
+// Example 1:
+
+// Input: nums = [1,1,0,1,1,1]
+// Output: 3
+// Explanation: The first two digits or the last three digits are consecutive 1s. The maximum number of consecutive 1s is 3.
+// Example 2:
+
+// Input: nums = [1,0,1,1,0,1]
+// Output: 2
 public class Maximum_Consecutive_1s {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -68,3 +80,4 @@ public class Maximum_Consecutive_1s {
         System.out.println("\n Space Complexity = O(1)");
     }
 }
+
